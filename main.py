c71390bbb72db3d44f856f99566c08df362a7c3f
Goo <110427078+arekune@users.noreply.github.com>
2022-11-02 11:28:06 +0200

    Define Notepad class for main.py, initialize root window and add main loop

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import tkinter
 
+class Notepad():
+
+    def __init__(self, master):
+        self.root = master
+        self.root.title("Stickypad")
+        self.root.iconbitmap("icons/sticky_note.ico")
+        self.root.geometry("600x600")
+    
+    def run_app(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    notepad = Notepad(root)
+    notepad.run_app()
