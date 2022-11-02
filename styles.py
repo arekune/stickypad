8226a3795f7b7a6d5264ef018049473dd23daad3
Goo <110427078+arekune@users.noreply.github.com>
2022-11-02 11:31:46 +0200

    Define fonts and colours in styles.py

---
+++ styles.py
@@ -0,0 +1,12 @@
+colours = {
+    "text_colour": "#fffacd",
+    "menu_colour": "#dbd9db",
+    "root_colour": "#6c809a"
+}
+
+font_families = ["Terminal", "Modern", "Script", "Courier", "Arial", "Calibri", "Cambria", "Georgia",
+                "MS Gothic", "SimSun", "Tahoma", "Times New Roman", "Verdana", "Wingdings"]
+
+font_sizes = [8, 10, 12, 14, 16, 20, 24, 32, 48, 64, 72, 96]
+
+font_options = ["normal", "bold", "italic"]
