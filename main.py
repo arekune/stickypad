3068dfa9be06bf31d659a9f1ed03697327ffd9fc
Goo <110427078+arekune@users.noreply.github.com>
2022-11-03 21:42:35 +0200

    Add exception handling

--- main.py
+++ main.py
@@ -297,7 +297,8 @@ class Notepad():
         The first three lines saved to the file are saved as font family, font size, and font option.
         Font size is converted to a string for the file.
 
-        Only the font configurations displayed in the font dropdown menus, font size sliderFINISH THIS!!!
+        Only the font configurations chosen when saving will be preserved and later applied for
+        a text file opened again as a note in Stickypad.
         """
 
         # Use filedialog (imported from tkinter) to ask user for file path and file name
@@ -308,13 +309,17 @@ class Notepad():
                                                 filetypes = (("Text Files", "*.txt"), ("All Files", "*.*")))
         
         # Write first three lines of font specification
-        with open(save_name, "w") as file:
-            file.write(self.font_family_drop.get() + "\n")
-            file.write(str(self.font_size.get()) + "\n")
-            file.write(self.font_option_drop.get() + "\n")
-        
-            # Write remaining text to the file with starting and ending indexes specified
-            file.write(self.text_input.get("1.0", END))
+        try:
+            with open(save_name, "w") as file:
+                file.write(self.font_family_drop.get() + "\n")
+                file.write(str(self.font_size.get()) + "\n")
+                file.write(self.font_option_drop.get() + "\n")
+            
+                # Write remaining text to the file with starting and ending indexes specified
+                file.write(self.text_input.get("1.0", END))
+        except FileNotFoundError:
+            # If no filename is given
+            pass
 
     def open_note(self):
         """
@@ -330,20 +335,25 @@ class Notepad():
             open_name = filedialog.askopenfilename(initialdir = "./",
                                                     title = "Open Note",
                                                     filetypes = (("Text Files", "*.txt"), ("All Files", "*.*")))
-            with open(open_name, "r+") as file:
-                self.text_input.delete("1.0", END)
+            
+            try:
+                with open(open_name, "r+") as file:
+                    self.text_input.delete("1.0", END)
 
-                # Newline characters at the end of the first three lines need to be stripped
-                self.font_family_drop.set(file.readline().strip())
-                self.font_size.set(int(file.readline().strip()))
-                self.font_option_drop.set(file.readline().strip())
+                    # Newline characters at the end of the first three lines need to be stripped
+                    self.font_family_drop.set(file.readline().strip())
+                    self.font_size.set(int(file.readline().strip()))
+                    self.font_option_drop.set(file.readline().strip())
 
-                # Call change_font function to implement the .set() methods, pass in arbitrary value
-                self.change_font(1)
+                    # Call change_font function to implement the .set() methods, pass in arbitrary value
+                    self.change_font(1)
 
-                # Read the rest of the file and insert into the text field
-                text = file.read()
-                self.text_input.insert("1.0", text)
+                    # Read the rest of the file and insert into the text field
+                    text = file.read()
+                    self.text_input.insert("1.0", text)
+            except FileNotFoundError:
+                # If no filename is given
+                pass
 
     def take_screenshot(self):
         x, y = self.text_input.winfo_rootx(), self.text_input.winfo_rooty()
